b3468aa23686d25a17998073ce1e775aa50e991c
Anish Das <135655078+ragingtitan@users.noreply.github.com>
2024-04-06 18:55:09 +0000

    added JARVIS GAMMA 5.0 and added necessary depen dependecies

---
+++ JARVIS GAMMA 5.0/connector.js
@@ -0,0 +1,16 @@
+/*const mysql=require('mysql2');
+let connection=mysql.createConnection({
+    host:"sql6.freemysqlhosting.net",
+    user:"sql6695220",
+    password:"cA7VUZBBJ1",
+    database:"sql6695220"
+});
+module.exports=connection;*/
+const mysql=require('mysql2');
+let connection=mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"28469",
+    database:"jarvis2"
+});
+module.exports=connection;
