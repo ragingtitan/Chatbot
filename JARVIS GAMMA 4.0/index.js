29a189f8aefdac564d3a7e633f586ca4b10ff6c1
Anish Das <135655078+ragingtitan@users.noreply.github.com>
2024-09-21 11:30:07 +0530

    Update index.js

--- JARVIS GAMMA 4.0/index.js
+++ JARVIS GAMMA 4.0/index.js
@@ -3,7 +3,6 @@
 let con=require('./connector')
 const express = require('express');
 const { GoogleGenerativeAI } = require("@google/generative-ai");
-const API_KEY='AIzaSyDw5Bu6_9erfCIF1BMMVMUIo8E3QI6yLk0';
 const bodyParser = require('body-parser');
 const path = require('path');
 const session=require("express-session");
